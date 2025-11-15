11f4d5c76d2993415ee99fc7603186b7fa081a4e
Avlasova Kateryna <demix11jr@gmail.com>
2025-11-15 19:51:37 +0200

    feature: add functionality for ServiceTypeForm and TransportTypeForm

--- ServiceTypeForm.cs
+++ ServiceTypeForm.cs
@@ -7,11 +7,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace TransportServiceApp
 {
     public partial class ServiceTypeForm : Form
     {
+        private string connectionString = @"Data Source=.;Initial Catalog=TransportServiceDB;Integrated Security=True";
+
         public ServiceTypeForm()
         {
             InitializeComponent();
@@ -19,14 +22,96 @@ namespace TransportServiceApp
 
         private void ServiceTypeForm_Load(object sender, EventArgs e)
         {
-            // TODO: данная строка кода позволяет загрузить данные в таблицу "transportServiceDBDataSet.ServiceType". При необходимости она может быть перемещена или удалена.
             this.serviceTypeTableAdapter.Fill(this.transportServiceDBDataSet.ServiceType);
-
         }
 
         private void toolStripButton1_Click(object sender, EventArgs e)
         {
-            this.serviceTypeTableAdapter.Update(this.transportServiceDBDataSet.ServiceType);
+            try
+            {
+                this.serviceTypeTableAdapter.Update(this.transportServiceDBDataSet.ServiceType);
+                MessageBox.Show("Дані успішно збережено!", "Успіх", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Помилка при збереженні: {ex.Message}", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // Пошук
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string searchText = txtSearch.Text.Trim();
+
+                if (string.IsNullOrEmpty(searchText))
+                {
+                    this.serviceTypeTableAdapter.Fill(this.transportServiceDBDataSet.ServiceType);
+                    lblStatus.Text = $"Показано всі {dataGridView1.RowCount} типів послуг";
+                    return;
+                }
+
+                string query = @"SELECT * FROM ServiceType 
+                                WHERE Name LIKE @SearchText
+                                ORDER BY Name";
+
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    SqlCommand command = new SqlCommand(query, connection);
+                    command.Parameters.AddWithValue("@SearchText", "%" + searchText + "%");
+
+                    SqlDataAdapter adapter = new SqlDataAdapter(command);
+                    DataTable dt = new DataTable();
+                    adapter.Fill(dt);
+
+                    dataGridView1.DataSource = dt;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Помилка при пошуку: {ex.Message}", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // Статистика
+        private void btnStats_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string query = @"SELECT COUNT(*) FROM ServiceType;";
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    SqlCommand command = new SqlCommand(query, connection);
+                    var result = command.ExecuteScalar();
+                    string stats = $"Статистика типів послуг:\n\n" +
+                             $"Всього типів: {result.ToString()}";
+
+                    MessageBox.Show(stats, "Статистика", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                    
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Помилка: {ex.Message}", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+ 
+
+        // Усі
+        private void btnShowAll_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.serviceTypeTableAdapter.Fill(this.transportServiceDBDataSet.ServiceType);
+                dataGridView1.DataSource = this.serviceTypeBindingSource;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Помилка при завантаженні даних: {ex.Message}", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
--- TransportTypeForm.cs
+++ TransportTypeForm.cs
@@ -7,11 +7,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace TransportServiceApp
 {
     public partial class TransportTypeForm : Form
     {
+        private string connectionString = @"Data Source=.;Initial Catalog=TransportServiceDB;Integrated Security=True";
+
         public TransportTypeForm()
         {
             InitializeComponent();
@@ -19,19 +22,98 @@ namespace TransportServiceApp
 
         private void TransportTypeForm_Load(object sender, EventArgs e)
         {
-            // TODO: данная строка кода позволяет загрузить данные в таблицу "transportServiceDBDataSet.TransportType". При необходимости она может быть перемещена или удалена.
             this.transportTypeTableAdapter.Fill(this.transportServiceDBDataSet.TransportType);
-
-        }
-
-        private void bindingNavigator1_RefreshItems(object sender, EventArgs e)
-        {
-
+            lblStatus.Text = $"Завантажено {dataGridView1.RowCount} типів транспорту";
         }
 
         private void toolStripButton1_Click(object sender, EventArgs e)
         {
-            this.transportTypeTableAdapter.Update(this.transportServiceDBDataSet.TransportType);
+            try
+            {
+                this.transportTypeTableAdapter.Update(this.transportServiceDBDataSet.TransportType);
+                MessageBox.Show("Дані успішно збережено!", "Успіх", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                lblStatus.Text = $"Збережено {dataGridView1.RowCount} типів транспорту";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Помилка при збереженні: {ex.Message}", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
+
+        // Пошук
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string searchText = txtSearch.Text.Trim();
+
+                if (string.IsNullOrEmpty(searchText))
+                {
+                    this.transportTypeTableAdapter.Fill(this.transportServiceDBDataSet.TransportType);
+                    return;
+                }
+
+                string query = @"SELECT * FROM TransportType 
+                                WHERE Name LIKE @SearchText
+                                ORDER BY Name";
+
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    SqlCommand command = new SqlCommand(query, connection);
+                    command.Parameters.AddWithValue("@SearchText", "%" + searchText + "%");
+
+                    SqlDataAdapter adapter = new SqlDataAdapter(command);
+                    DataTable dt = new DataTable();
+                    adapter.Fill(dt);
+
+                    dataGridView1.DataSource = dt;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Помилка при пошуку: {ex.Message}", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // Статистика
+        private void btnStats_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string query = @"SELECT COUNT(*) FROM TransportType;";
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    SqlCommand command = new SqlCommand(query, connection);
+                    var result = command.ExecuteScalar();
+                    string stats = $"Статистика типів автотранспорту:\n\n" +
+                             $"Всього типів: {result.ToString()}";
+
+                    MessageBox.Show(stats, "Статистика", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Помилка: {ex.Message}", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // Усі
+        private void btnShowAll_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.transportTypeTableAdapter.Fill(this.transportServiceDBDataSet.TransportType);
+                dataGridView1.DataSource = this.transportTypeBindingSource;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Помилка при завантаженні даних: {ex.Message}", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+
     }
 }
